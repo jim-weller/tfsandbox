86a4a1478dfb171a58c40ae8a844ee6ef48997cc
Jim Weller <jim.weller@hyland.com>
2023-12-08 11:37:26 -0800

    Fri Dec  8 11:37:26 PST 2023

--- main.tf
+++ main.tf
@@ -6,17 +6,17 @@ terraform {
     }
   }
 
-  required_version = ">= 1.2.0"
+required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "example_server" {
   ami           = "ami-093467ec28ae4fe03"
   instance_type = "t2.micro"
-  subnet_id = "subnet-5eaa0807"
+  subnet_id     = "subnet-5eaa0807"
   tags = {
     Name = "BlahBlah"
   }
