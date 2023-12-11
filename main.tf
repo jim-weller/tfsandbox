1a360b9f4f104642584ea59bf864f45b645fee4b
Jim Weller <jim.weller@hyland.com>
2023-12-11 11:10:25 -0800

    Mon Dec 11 11:10:25 PST 2023

--- main.tf
+++ main.tf
@@ -9,15 +9,65 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+provider "random" {}
+
 provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example_server" {
-  ami           = "ami-093467ec28ae4fe03"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-5eaa0807"
+terraform {
+  backend "s3" {
+    bucket = "tfstate-ee03fdccbeb4bf4177b97d1c3289b2ab06089789"
+    key    = "tfsandbox.tfstate"
+    region = "us-west-2"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*-x86_64-ebs"]
+  }
+}
+
+resource "random_pet" "my_pet" {
+    length = 2
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "BlahBlah"
+    Name = random_pet.my_pet.id  
+  }
+}
+
+resource "aws_vpc_dhcp_options" "example_dhcp_options" {
+  domain_name = "local"
+  tags = {
+    Name = random_pet.my_pet.id  
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "dns_resolver" {
+  vpc_id          = aws_vpc.example_vpc.id
+  dhcp_options_id = aws_vpc_dhcp_options.example_dhcp_options.id
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = random_pet.my_pet.id  
+  }
+}
+
+resource "aws_instance" "example_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example_subnet.id
+  tags = { 
+    Name = random_pet.my_pet.id  
   }
 }
